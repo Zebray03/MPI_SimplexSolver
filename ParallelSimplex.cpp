9480a04f527f433e6b87b7cf3083637982d5b3fb
zebray <2092680295@qq.com>
2024-05-29 20:04:02 +0800

    5.29 submit

--- ParallelSimplex.cpp
+++ ParallelSimplex.cpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "Matrix.h"
+#include <mpi.h>
+
 class ParallelSimplex
 {
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "ParallelSimplex.cpp"
 
-int main()
-{
+int main(int argc, char* argv[]) {
 
+	return 0;
 }
